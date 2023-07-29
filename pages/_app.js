f0b2ce8eba4f2585729f546d88b55016b9c136fc
funkiirabu <funkiirabu@gmail.com>
2023-07-29 12:14:02 +1000

    Adding suiet wallet connect

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import '@/styles/globals.css'
+import { WalletProvider } from '@suiet/wallet-kit';
+import '@suiet/wallet-kit/style.css';
+import ConnectWallet from '../components/ConnectWallet';  // adjust the path if needed
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  return (
+    <WalletProvider>
+      <Component {...pageProps} />
+      <ConnectWallet />
+    </WalletProvider>
+  );
 }
+
+export default App;
